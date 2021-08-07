c4e8cadb9c5509e5dfe1e3b4d47b490eec47a071
lucasdevelop <lucas.palomo@t-online.de>
2021-08-07 16:32:56 +0200

    rename

--- test/core.jsste.test.js
+++ test/core.jsste.test.js
@@ -1,10 +1,10 @@
 
 let  core_render = require("./core.render.test");
-let  core_scriptexec = require("./scriptexec.jste.test");
+let  core_scriptexec = require("./scriptexec.jsste.test");
 
 function test()
 {
-    describe("JSTE CORE", function () {
+    describe("JSSTE CORE", function () {
       
       core_render();
       core_scriptexec();
