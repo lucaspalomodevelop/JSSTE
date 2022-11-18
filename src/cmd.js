60e26ffb37cd80c95972fe05402fe31c0589fb84
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-11-18 16:58:42 +0100

    add log mode in cmd

--- src/cmd.js
+++ src/cmd.js
@@ -1,10 +1,10 @@
 let jsste = require("./index");
 let myargs = process.argv.slice(2);
-let path = require("path");
-const { fstat } = require("fs");
 let output = "";
 let pagefile, tempfile;
 let fs = require("fs");
+
+
 
 let addCommand = ({prefix,args = myargs}, callback) =>
 {
@@ -17,23 +17,26 @@ let addCommand = ({prefix,args = myargs}, callback) =>
     })
 }
 
+addCommand({prefix:"-log"},(arg) =>{
+    jsste.setStateFunction((state) =>{
+        console.log(state);
+    })
+})
+
 addCommand({prefix:"-Jsconfig="},(arg) =>{
     jsste.__config.setConfig(arg);
 })
 
 addCommand({prefix:"-page="},(arg) =>{
-    
     pagefile = JSON.parse(fs.readFileSync(arg,"utf-8").toString())
-    console.log(pagefile)
 })
 
 addCommand({prefix:"-temp="},(arg) =>{
     tempfile  = fs.readFileSync(arg, "utf-8").toString()
-    console.log(tempfile)
 })
 
 output = jsste.render(pagefile || undefined, tempfile || undefined)
 
 addCommand({prefix:"-out"},(arg) =>{
-    console.log("output\n", output )
+    console.log(output)
 })
